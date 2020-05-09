94eb938b13bc01ac83c509bb8c1b350c66f2265f
Brendon Hampton <brendonhampton1988@gmail.com>
2020-05-08 19:16:07 -0700

    added eslintrc, ejs, schema, connected database

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is a project for my portfolio showcasing my projects and what i have build
 
 ### Credits
     1. https://meyerweb.com/eric/tools/css/reset/ ---Meyers reset.css
+
+### 
--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-'use strict';
+
 
 require('dotenv').config();
-// const cors = require('cors');
 const express = require('express');
-
 const app = express();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+
+
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors());
+app.use(express.urlencoded({extended:true,}));
 app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+// routes to ejs pages
 
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
-app.listen( PORT, () => console.log('The server is up on', PORT));
+// home
+app.get('/home');
+
+// bio
+
+// projects
+
+// Force Error
+//404 Not Found
+// 500 error handler
+
+
+// server start up
+function startServer(PORT) {
+  app.listen( PORT, () => console.log('The server is up on', PORT));
+}
+
+client.connect()
+  .then(() => {
+    startServer(PORT);
+  })
+  .catch( error => console.error(error.message));
